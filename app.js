131b042043ddeabbcb4cb7d3f29df4a7ce22870b
carmencitaap <cmavendano@miuandes.cl>
2021-05-23 19:36:22 -0400

    inputs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {title} = require('./view')
+const {title, input} = require('./view')
 const {table} = require('./view')
 const {printTable} = require('console-table-printer')
 
@@ -7,7 +7,7 @@ model = {
     leftUnit: "Celsius",
     rightValue: 32,
     rightUnit: "Farenheit",
-    input1: 0,
+    input1: '',
     input2: 0,
     input3: 0,
     input4: 0
@@ -16,3 +16,4 @@ model = {
 
 console.log(title())
 printTable(table(model))
+const {input1,input2,input3,input4} = input(model)
--- view.js
+++ view.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const figlet = require('figlet')
+const inquirer = require('inquirer')
 
 function title() {
     return chalk.magenta(
@@ -24,7 +25,53 @@ function table(model) {
     ]
 }
 
+function input(model) {
+    const {input1} = model
+    const {input2} = model
+    const {input3} = model
+    const {input4} = model
+
+    const message1 = "Left temperature is source? "
+    const message2 = "Temperature value to convert? "
+    const message3 = "From? "
+    const message4 = "To?"
+
+    const choices = ["Celsius", "Farenheit", "Kelvin"]
+
+    return inquirer.prompt([
+        {
+            name: "input1",
+            type: "input",
+            message: message1,
+            default: input1
+        },
+        {
+            name: "input2",
+            type: "int",
+            message: message2,
+            default: input2
+        },
+        {
+            name: "input3",
+            type: "list",
+            message: message3,
+            default: input3,
+            choices : choices
+        },
+        {
+            name: "input4",
+            type: "list",
+            message: message4,
+            default: input4,
+            choices : choices
+        }
+        
+    ])
+
+}
+
 module.exports = {
     title,
-    table
+    table,
+    input
 }
